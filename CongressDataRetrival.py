14f2d0e3f452d4c074a17e140670643d2ff28cf9
PenningtonStuart <stuart.lee.pennington@gmail.com>
2021-06-17 17:57:52 -0500

    Working file up to directory creation

---
+++ CongressDataRetrival.py
@@ -0,0 +1,50 @@
+#Retrieve Data from Congress Application and format it as JSON
+
+import csv, json, zipfile
+import requests, PyPDF2
+import os
+from datetime import datetime
+
+
+#House of Represenatives
+
+#globals
+current_year = str(datetime.today().year)
+current_directory = os.getcwd()
+
+#get last name of represenative from user
+represenative_name = input("Represenative's Last Name: ")
+
+house_zip_url = "https://disclosures-clerk.house.gov/public_disc/financial-pdfs/" + current_year + "FD.ZIP"
+house_pdf_base_url = "https://disclosures-clerk.house.gov/public_disc/ptr-pdfs/" + current_year + "/"
+
+zip_file_request = requests.get(house_zip_url)
+new_zipfile_name = represenative_name + current_year + ".zip"
+
+#download zip file 
+with open(new_zipfile_name, 'wb') as file:
+    file.write(zip_file_request.content)
+
+with zipfile.ZipFile(new_zipfile_name) as z:
+    z.extractall('.')
+
+with open('2021FD.txt') as f:
+    for line in csv.reader(f, delimiter = '\t'):
+        
+        #create directory for represenative if it doesn't exist
+        if not os.path.exists(current_directory + "/" + represenative_name):
+             try:
+                os.makedirs(current_directory + "/" + represenative_name)
+             except OSError as exc: # Guard against race condition
+                if exc.errno != zipfile.error.EEXIST:
+                    raise
+
+        if line[1] == represenative_name:
+            date = line[7]
+            doc_id = line[8]
+            
+            pdf_doc_source = requests.get(f"{house_pdf_base_url}{doc_id}.pdf")
+            
+            #Write newest file under Represenative's name
+            with open(f"/{represenative_name}/{doc_id}.pdf", 'wb') as pdf_file:
+                pdf_file.write(pdf_doc_source.content)
